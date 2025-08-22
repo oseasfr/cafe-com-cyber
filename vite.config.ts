f4d797571bdb262b73006aba04f19181c80ec71e
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-21 21:09:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode } ) => ({
+  base: "/cafe-com-cyber-hub/", // <--- CORREÇÃO ADICIONADA AQUI
   server: {
     host: "::",
     port: 8080,
