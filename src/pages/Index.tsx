859f56ed9f590801456296189092332b21df3eef
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-23 23:00:24 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,9 +7,9 @@ import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen">
       <Header />
-      <main>
+      <main className="bg-background">
         <HeroSection />
         <ArticlesSection />
         <DownloadsSection />
