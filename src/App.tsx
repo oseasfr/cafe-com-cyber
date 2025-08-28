b9a7575977bb1b9a9de795cbadecaf76f6ac1b2e
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-28 14:27:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,37 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Importação das páginas
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import UnderConstruction from "./pages/UnderConstruction";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            {/* Página inicial */}
+            <Route path="/" element={<Index />} />
+            
+            {/* Página em construção */}
+            <Route path="/em-construcao" element={<UnderConstruction />} />
+            
+            {/* ADICIONE SUAS NOVAS ROTAS AQUI, SEMPRE ACIMA DA ROTA 404 */}
+            
+            {/* Página 404 - SEMPRE deve ser a última rota */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
