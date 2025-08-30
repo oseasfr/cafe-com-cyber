782b15716853b5d92925dead72f8664d5aa15368
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-29 23:40:24 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import UnderConstruction from "./pages/UnderConstruction";
-import ArticlePage from "./pages/articles/ArticlePage";
 
 const queryClient = new QueryClient();
 
@@ -20,24 +19,13 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
-            {/* Opção 1: Deixar o site online.
-              Basta descomentar a linha abaixo. 
-            */}
+            {/* Rota principal que carrega a página inicial */}
             <Route path="/" element={<Index />} />
-            
-            {/* Opção 2: Colocar o site em manutenção.
-              - Comente a linha da "Opção 1" acima.
-              - Descomente a linha abaixo.
-            */}
-            {/* <Route path="/" element={<Navigate to="/em-construcao" replace />} /> */}
-            
-            {/* Rota para os artigos. */}
-            <Route path="/articles/:articleId" element={<ArticlePage />} />
 
-            {/* Rota da página de manutenção. É necessária para que o redirecionamento funcione. */}
+            {/* A página "em-construcao" pode ser acessada por esta rota */}
             <Route path="/em-construcao" element={<UnderConstruction />} />
-            
-            {/* Rota 404 para URLs não encontradas. */}
+
+            {/* Página 404 - SEMPRE deve ser a última rota */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
