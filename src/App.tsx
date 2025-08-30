95052c6a88d7595bb516eb9c1a3ecea9a0bcd117
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-30 01:11:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 // Importação das páginas
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import UnderConstruction from "./pages/UnderConstruction";
-import ArticlePage from "./pages/articles/ArticlePage";
-import ArticlesArchive from "./pages/articles/ArticlesArchive";
 
 // Componente para rolar a página para o topo em cada mudança de rota
 const ScrollToTop = () => {
@@ -35,20 +31,11 @@ const App = () => {
         <BrowserRouter>
           <ScrollToTop /> {/* Adicionado para forçar a rolagem para o topo */}
           <Routes>
-            {/* Rota principal que carrega a página inicial 
-            <Route path="/" element={<Index />} />*/}
+            {/* Rota principal que carrega a página de manutenção */}
+            <Route path="/" element={<UnderConstruction />} />
             
-            {/* Rota para os artigos, com um ID dinâmico */}
-            <Route path="/articles/:articleId" element={<ArticlePage />} />
-
-            {/* A página "em-construcao" pode ser acessada por esta rota */}
-            <Route path="/em-construcao" element={<UnderConstruction />} />
-            
-            {/* Rota para a página de arquivo de artigos */}
-            <Route path="/articles" element={<ArticlesArchive />} />
-
-            {/* Página 404 - SEMPRE deve ser a última rota */}
-            <Route path="*" element={<NotFound />} />
+            {/* Todas as outras rotas redirecionam para a página de manutenção */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
