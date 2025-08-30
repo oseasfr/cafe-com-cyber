45726b4d9feff6f9a189d845965cfc389b393e3b
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-29 23:58:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ArticlesArchive from "./pages/articles/ArticlesArchive";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
