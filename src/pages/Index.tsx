5a79cb626bea8eab2e5b1abcabaa174143d27f8a
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-29 23:16:38 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import ArticlesSection from "@/components/ArticlesSection";
+import { ArticlesSection } from "@/components/ArticlesSection";
 import DownloadsSection from "@/components/DownloadsSection";
 import CommunitySection from "@/components/CommunitySection";
 import Footer from "@/components/Footer";
