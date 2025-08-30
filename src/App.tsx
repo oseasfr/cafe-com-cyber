7d14e0950b080a6c7c069e1603be71450a1957fb
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-08-30 01:02:37 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
 // Importação das páginas
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import UnderConstruction from "./pages/UnderConstruction";
 import ArticlePage from "./pages/articles/ArticlePage";
 import ArticlesArchive from "./pages/articles/ArticlesArchive";
-
-// Componente para rolar a página para o topo em cada mudança de rota
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
-
-const queryClient = new QueryClient();
 
 const App = () => {
   return (
