b5cddf0a3fc3d168968c8daa5ddd6b56d7c30728
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-09-05 23:39:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import ArticlesArchive from "./pages/articles/ArticlesArchive";
 import CommunityPage from "./pages/CommunityPage"; // Adicionado: Importação da página da comunidade
 
 // *** Variável de controle: Altere para 'false' para desativar a página de manutenção ***
-const MAINTENANCE_MODE = false;
+const MAINTENANCE_MODE = true;
 
 // Componente para rolar a página para o topo em cada mudança de rota
 const ScrollToTop = () => {
