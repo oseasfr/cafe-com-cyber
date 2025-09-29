38f8ee0649d7def7455ed14881656f99370561c2
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-09-28 21:24:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import ArticlePage from "./pages/articles/ArticlePage";
 import ArticlesArchive from "./pages/articles/ArticlesArchive";
 import CommunityPage from "./pages/CommunityPage"; // Adicionado: Importação da página da comunidade
 // NOVO: Importação do componente da nova página
-import Usefullinks from "./pages/links-uteis"; 
+import UsefulLinks from "./pages/useful-links"; 
 
 // *** Variável de controle: Altere para 'false' para desativar a página de manutenção ***
 const MAINTENANCE_MODE = false;
