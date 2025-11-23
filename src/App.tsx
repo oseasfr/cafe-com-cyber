0ca8f733b47706f95de774f52d90e676abbba60c
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-11-23 00:00:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,70 +5,88 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+
+// Importe todos os seus componentes de página
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import UnderConstruction from "./pages/UnderConstruction";
 import ArticlePage from "./pages/articles/ArticlePage";
 import ArticlesArchive from "./pages/articles/ArticlesArchive";
 import CommunityPage from "./pages/CommunityPage";
-import UsefulLinks from "./pages/useful-links"; 
-const MAINTENANCE_MODE = true;
+import UsefulLinks from "./pages/useful-links";
+
+// NOVO IMPORT: Componente GeradorDeSenhas
+import GeradorDeSenhas from "./pages/GeradorDeSenhas"; 
+
+// 🚨 CONTROLE DE MANUTENÇÃO
+const MAINTENANCE_MODE = true; 
+
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
 };
+
 const queryClient = new QueryClient();
+
 const App = () => {
-  if (MAINTENANCE_MODE) {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="*" element={<UnderConstruction />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </QueryClientProvider>
-    );
-  }
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            {/* Rota principal que carrega a página inicial */}
-            <Route path="/" element={<Index />} />
-            
-            {/* Rota para os artigos, com um ID dinâmico */}
-            <Route path="/articles/:articleId" element={<ArticlePage />} />
+  // BLOCO 1: SE MANUTENÇÃO ESTÁ ATIVA (TRUE)
+  if (MAINTENANCE_MODE) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              {/* TUDO VAI PARA EM CONSTRUÇÃO, incluindo /gerador-de-senhas */}
+              <Route path="*" element={<UnderConstruction />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    );
+  }
 
-            {/* Rota para a página de arquivo de artigos */}
-            <Route path="/articles" element={<ArticlesArchive />} />
+  // BLOCO 2: SE MANUTENÇÃO ESTÁ DESATIVADA (FALSE)
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            {/* Rota principal que carrega a página inicial */}
+            <Route path="/" element={<Index />} />
+            
+            {/* Rota correta para o Gerador de Senhas */}
+            <Route path="/gerador-de-senhas" element={<GeradorDeSenhas />} />
 
-            {/* Rota para a página da comunidade */}
-            <Route path="/community" element={<CommunityPage />} />
-            
-            {/* NOVO: Rota para o seu dashboard Dashy */}
-            <Route path="/useful-links" element={<UsefulLinks />} />
+            {/* Rota para os artigos, com um ID dinâmico */}
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
 
-            {/* A página "em-construcao" pode ser acessada por esta rota */}
-            <Route path="/em-construcao" element={<UnderConstruction />} />
-            
-            {/* Página 404 - SEMPRE deve ser a última rota */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+            {/* Rota para a página de arquivo de artigos */}
+            <Route path="/articles" element={<ArticlesArchive />} />
+
+            {/* Rota para a página da comunidade */}
+            <Route path="/community" element={<CommunityPage />} />
+            
+            {/* Rota para os links úteis */}
+            <Route path="/useful-links" element={<UsefulLinks />} />
+
+            {/* A página "em-construcao" pode ser acessada por esta rota */}
+            <Route path="/em-construcao" element={<UnderConstruction />} />
+            
+            {/* Página 404 - SEMPRE deve ser a última rota */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
 };
+
 export default App;
