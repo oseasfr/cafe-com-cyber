868493f889b5a5120ddf0a0705c9cb483a47a981
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-11-23 23:03:01 -0300

    Update gerador-de-senhas.tsx

--- src/pages/gerador-de-senhas.tsx
+++ src/pages/gerador-de-senhas.tsx
@@ -1,15 +1,17 @@
+import React from 'react'
+
 export default function GeradorDeSenhas() {
   return (
     <iframe
       src="https://gerador-de-senhas-cme.pages.dev/"
       title="Gerador de Senhas"
       style={{
-        position: "absolute",
+        position: 'absolute',
         top: 0,
         left: 0,
-        width: "100%",
-        height: "100%",
-        border: "none",
+        width: '100%',
+        height: '100%',
+        border: 'none',
       }}
     />
   )
