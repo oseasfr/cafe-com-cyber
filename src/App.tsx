a101d5bd36ff04e74d1c98a1cad61eb9c5063ff7
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-12-22 21:07:47 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import NotFound from "./pages/NotFound";
 import UnderConstruction from "./pages/UnderConstruction";
 import ArticlePage from "./pages/articles/ArticlePage";
 import ArticlesArchive from "./pages/articles/ArticlesArchive";
-import Comunidade from "./pages/Comunidade";
+import ComunidadePagina from "./pages/Comunidade";
 import LinksUteis from "./pages/links-uteis";
 import GeradorDeSenhas from "./pages/gerador-de-senhas"; 
 
@@ -57,7 +57,7 @@ const App = () => {
             <Route path="/gerador-de-senhas" element={<GeradorDeSenhas />} />
             <Route path="/articles/:articleId" element={<ArticlePage />} />
             <Route path="/articles" element={<ArticlesArchive />} />
-            <Route path="/Comunidade" element={<ComunidadeSessao />} />
+            <Route path="/Comunidade" element={<ComunidadePagina />} />
             <Route path="/links-uteis" element={<LinksUteis />} />
             <Route path="/em-construcao" element={<UnderConstruction />} />
             <Route path="*" element={<NotFound />} />
