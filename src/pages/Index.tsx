9bc7db49c512d617e06a9e90fc359190b015e7e9
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-12-24 14:02:26 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import ArticlesSection from "@/components/ArticlesSection";
-import DownloadsSection from "@/components/DownloadsSection";
 import CommunitySection from "@/components/CommunitySection";
+import AboutSection from "@/components/AboutSection";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -12,8 +12,8 @@ const Index = () => {
       <main role="main">
         <HeroSection />
         <ArticlesSection />
-        <DownloadsSection />
         <CommunitySection />
+        <AboutSection />
       </main>
       <Footer />
     </div>
