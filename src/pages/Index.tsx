c76509e6dc7e32b85d780903e518be8ef0a84476
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-12-23 23:50:30 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <main>
+      <main role="main">
         <HeroSection />
         <ArticlesSection />
         <DownloadsSection />
