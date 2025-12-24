181e4f833cd381da1210e34cfea5ca08144756c4
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-12-23 22:13:23 -0300

    Update ArtigosPagina.tsx

--- src/pages/artigos/ArtigosPagina.tsx
+++ src/pages/artigos/ArtigosPagina.tsx
@@ -3,17 +3,12 @@ import { useParams } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import NotFound from '../NotFound';
-// ALTERAÇÃO 1: Importação de dados renomeada
 import { artigos } from '../../data/artigos'; 
-// Supondo que você tenha um componente para o conteúdo do artigo
-import ArticleContent from '@/components/ArticleContent'; 
+// CORREÇÃO AQUI: ArticleContent -> ArtigoConteudo
+import ArtigoConteudo from '@/components/ArtigoConteudo'; 
 
-// ALTERAÇÃO 2: Renomeação do componente
 const ArtigosPagina = () => {
-  // ALTERAÇÃO 3: Renomeação do parâmetro de rota
   const { artigoId } = useParams(); 
-  
-  // ALTERAÇÃO 4: Renomeação da variável
   const artigo = artigos.find(a => a.id === artigoId); 
 
   if (!artigo) {
@@ -24,13 +19,12 @@ const ArtigosPagina = () => {
     <div>
       <Header />
       <main>
-        {/* ALTERAÇÃO 5: Uso da variável renomeada */}
-        <ArticleContent artigo={artigo} /> 
+        {/* CORREÇÃO AQUI: ArticleContent -> ArtigoConteudo */}
+        <ArtigoConteudo artigo={artigo} /> 
       </main>
       <Footer />
     </div>
   );
 };
 
-// ALTERAÇÃO 6: Renomeação do export
 export default ArtigosPagina;
