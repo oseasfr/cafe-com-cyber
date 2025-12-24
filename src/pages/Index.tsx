1b49252effc4a48ce375cc6185724deb20f09216
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2025-12-23 23:08:17 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import ArtigosSessao from "@/components/ArtigosSessao";
+import ArticlesSection from "@/components/ArticlesSection";
 import DownloadsSection from "@/components/DownloadsSection";
 import CommunitySection from "@/components/CommunitySection";
 import Footer from "@/components/Footer";
@@ -11,7 +11,7 @@ const Index = () => {
       <Header />
       <main>
         <HeroSection />
-        <ArtigosSessao />
+        <ArticlesSection />
         <DownloadsSection />
         <CommunitySection />
       </main>
