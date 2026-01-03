6877c5659c38f65dba832a6c880cf1bcc69132ec
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-01-03 00:55:54 -0300

    Update CommunitySection.tsx

--- src/components/CommunitySection.tsx
+++ src/components/CommunitySection.tsx
@@ -107,7 +107,7 @@ const CommunitySection = () => {
               }
               >
               <MessageCircle className="mr-2 h-5 w-5" />
-              Participar do Grupo do Whatsapp
+              Grupo do Whatsapp
             </Button>
           </CardContent>
         </Card>
