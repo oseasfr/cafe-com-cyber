01e13b3a6d0560d4d98a4214622d46080927528e
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-01-03 22:49:45 -0300

    Update articles.ts

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -5,6 +5,7 @@ import artigo1 from '../content/articles/fundamentos-de-seguranca-em-apis-rest.m
 import artigo2 from '../content/articles/zero-trust-o-futuro-da-seguranca-corporativa.md?raw';
 import artigo3 from '../content/articles/osint-tecnicas-de-investigacao-digital.md?raw';
 import artigo4 from '../content/articles/guia-para-analise-de-malwares-em-sistemas-linux.md?raw';
+import artigo5 from '../content/articles/quando-foi-a-ultima-vez-que-voce-alterou-sua-senha.md?raw';
 
 // Carrega todos os artigos usando o loader
 export const articles = loadArticles([artigo1, artigo2, artigo3, artigo4]);
