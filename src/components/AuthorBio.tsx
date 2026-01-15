6d9f3023114ad4ead90b5c262a5658ed34038f33
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-01-14 22:50:18 -0300

    Update AuthorBio.tsx

--- src/components/AuthorBio.tsx
+++ src/components/AuthorBio.tsx
@@ -1,30 +1,33 @@
-import { Link } from 'react-router-dom';
+  import { Link } from 'react-router-dom';
+  import ScrollReveal from "@/components/ScrollReveal";
 
-interface AuthorBioProps {
-  author: string;
-  date: string;
-  readTime: string;
-  authorImage?: string; // Imagem do autor é opcional
-}
+  interface AuthorBioProps {
+    author: string;
+    date: string;
+    readTime: string;
+    authorImage?: string; // Imagem do autor é opcional
+  }
 
-export const AuthorBio = ({ author, date, readTime, authorImage }: AuthorBioProps) => {
-  return (
-    <div className="flex items-center space-x-4 mb-8">
-      {authorImage && (
-        <img
-          src={authorImage}
-          alt={`Foto de ${author}`}
-          className="w-12 h-12 rounded-full object-cover"
-        />
-      )}
-      <div className="flex flex-col">
-        <span className="text-lg font-semibold text-foreground">{author}</span>
-        <div className="flex items-center text-sm text-muted-foreground space-x-2">
-          <span>{date}</span>
-          <span>•</span>
-          <span>{readTime}</span>
+  export const AuthorBio = ({ author, date, readTime, authorImage }: AuthorBioProps) => {
+    return (
+      <ScrollReveal>
+        <div className="flex items-center space-x-4 mb-8">
+          {authorImage && (
+            <img
+              src={authorImage}
+              alt={`Foto de ${author}`}
+              className="w-12 h-12 rounded-full object-cover"
+            />
+          )}
+          <div className="flex flex-col">
+            <span className="text-lg font-semibold text-foreground">{author}</span>
+            <div className="flex items-center text-sm text-muted-foreground space-x-2">
+              <span>{date}</span>
+              <span>•</span>
+              <span>{readTime}</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+      </ScrollReveal>
+    );
+  };
