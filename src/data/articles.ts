d83abc987fcc455d7fe63e1d5d89c19cfbb136cb
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-01-29 20:32:59 -0300

    Update articles.ts

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -1,13 +1,13 @@
 import { loadArticles } from '../lib/articleLoader';
 
 // Importa os arquivos .md como texto bruto usando ?raw
-import artigo1 from '../content/articles/guia-para-analise-de-malwares-em-sistemas-linux.md?raw';
-import artigo2 from '../content/articles/quando-foi-a-ultima-vez-que-voce-alterou-sua-senha.md?raw';
-import artigo3 from '../content/articles/external-secrets-k8s-vault-hashicorp-ldap-postgres.md?raw';
-import artigo4 from '../content/articles/monitoramento-continuo-o-papel-estrategico-na-seguranca-da-informacao-corporativa.md?raw';
+// import artigo1 from '../content/articles/guia-para-analise-de-malwares-em-sistemas-linux.md?raw';
+import artigo1 from '../content/articles/quando-foi-a-ultima-vez-que-voce-alterou-sua-senha.md?raw';
+// import artigo3 from '../content/articles/external-secrets-k8s-vault-hashicorp-ldap-postgres.md?raw';
+// import artigo4 from '../content/articles/monitoramento-continuo-o-papel-estrategico-na-seguranca-da-informacao-corporativa.md?raw';
 
 // Carrega todos os artigos usando o loader
-export const articles = loadArticles([artigo1, artigo2, artigo3, artigo4]);
+export const articles = loadArticles([artigo1]);
 
 // Exporta tipos para uso em outros arquivos
 export type { Article, ArticleMetadata } from '../lib/articleLoader';
